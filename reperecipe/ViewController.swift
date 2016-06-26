bcf3c5a5d7ea7365bfb9e2c96f757bec0d72a918
shikichee <t.shikichi125@gmail.com>
2016-06-26 13:19:51 +0900

    initial commit

---
+++ reperecipe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  reperecipe
+//
+//  Created by 敷地琢也 on 2016/06/26.
+//  Copyright © 2016年 Takuya Shikichi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
