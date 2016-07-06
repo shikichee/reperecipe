38281fad08c8140b5d5e2425e2c2665af53f7160
shikichee <t.shikichi125@gmail.com>
2016-07-06 23:41:14 +0900

    Fix auto layout in myrecipe

--- reperecipe/RecipeCell.swift
+++ reperecipe/RecipeCell.swift
@@ -17,7 +17,8 @@ class RecipeCell: UITableViewCell {
         super.layoutSubviews()
         recipeView.layer.borderWidth = 1.0
         recipeView.layer.borderColor = ReperecipeColor.Line.normal.CGColor
-        recipeView.layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        recipeView.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
         recipeView.layer.shadowOpacity = 0.5
+        recipeView.layer.shadowRadius = 1.0
     }
 }
