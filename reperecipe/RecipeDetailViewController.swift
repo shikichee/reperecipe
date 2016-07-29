f49b3e90c0bb53033184b8194e04627c32df2d35
shikichee <t.shikichi125@gmail.com>
2016-07-29 23:33:31 +0900

    Fix recipeDetail error

--- reperecipe/RecipeDetailViewController.swift
+++ reperecipe/RecipeDetailViewController.swift
@@ -20,17 +20,18 @@ class RecipeDetailViewController: UIViewController {
     
     @IBOutlet weak var ingredientLabel: UILabel!
     @IBOutlet weak var refrigeratorLabel: UILabel!
+    
     @IBOutlet weak var memoTextView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
-        tableView.registerNib(UINib(nibName: "IngredientCellOfRecipeDetail", bundle: nil), forCellReuseIdentifier: "IngredientCellOfRecipeDetail")
+        
         if let image = recipe.image {
             recipeImageView.image = image
         } else {
-            recipeImageView.hidden = true
+            recipeImageView.frame = true
         }
         categoryImageView.image = RecipeViewModel.categories()[recipe.categoryId].image
         nameLabel.text = recipe.name
@@ -67,7 +68,7 @@ extension RecipeDetailViewController: UITableViewDataSource {
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("IngredientCellOfRecipeDetail", forIndexPath: indexPath) as! IngredientCellOfRecipeDetail
+        let cell = tableView.dequeueReusableCellWithIdentifier(R.reuseIdentifier.ingredientCellOfRecipeDetail.identifier) as! IngredientCellOfRecipeDetail
         let ingredient = recipe.ingredients[indexPath.row]
         cell.nameLabel.text = ingredient.name
         cell.quantityLabel.text = ingredient.quantityText
