14c65e4b87d9a501927ed639f91d8c92a258e1b4
shikichee <t.shikichi125@gmail.com>
2016-08-03 23:36:06 +0900

    Add shopping cell

---
+++ reperecipe/ShoppingIngredientCell.swift
@@ -0,0 +1,14 @@
+//
+//  ShoppingIngredientCell.swift
+//  reperecipe
+//
+//  Created by Takuya Shikichi on 2016/08/03.
+//  Copyright © 2016年 Takuya Shikichi. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingIngredientCell: UITableViewCell {
+    
+    
+}
--- reperecipe/ShoppingPagingViewController.swift
+++ reperecipe/ShoppingPagingViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ShoppingPagingViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
     class func instantiateFromStoryboard() -> ShoppingPagingViewController {
         let storyboard = UIStoryboard(name: "PagingViewController", bundle: nil)
         return storyboard.instantiateViewControllerWithIdentifier(String(self)) as! ShoppingPagingViewController
@@ -17,10 +18,43 @@ class ShoppingPagingViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let nib = UINib(nibName: R.nib.shoppingSectionHeader.name, bundle: nil)
+        tableView.registerNib(nib, forHeaderFooterViewReuseIdentifier: "ShoppingSectionHeader")
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
+
+extension ShoppingPagingViewController: UITableViewDelegate {
+    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        // Here, we use NSFetchedResultsController
+        // And we simply use the section name as title
+//        let currSection = fetchedResultsController.sections?[section]
+//        let title = currSection!.name
+//        
+        // Dequeue with the reuse identifier
+        let cell = self.tableView.dequeueReusableHeaderFooterViewWithIdentifier("ShoppingSectionHeader")
+//        let header = cell as! ShoppingSectionHeader
+//        header.titleLabel.text = title
+        
+        return cell
+    }
+}
+
+extension ShoppingPagingViewController: UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell")
+        cell.
+        
+        return cell
+    }
+    
+
 }
